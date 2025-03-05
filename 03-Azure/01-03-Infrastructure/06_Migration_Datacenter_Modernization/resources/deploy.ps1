502e07507d86d192b966a02837f3b973a575c4a8
Donovan Hughes <dohughes@microsoft.com>
2025-03-05 22:19:14 +0100

    Fix deploy.ps1

--- 03-Azure/01-03-Infrastructure/06_Migration_Datacenter_Modernization/resources/deploy.ps1
+++ 03-Azure/01-03-Infrastructure/06_Migration_Datacenter_Modernization/resources/deploy.ps1
@@ -1,5 +1,5 @@
 # Setting Variables
-$pathToRepo = "https://raw.githubusercontent.com/microsoft/MicroHack/main/03-Azure/01-03-Infrastructure/06_Migration_Datacenter_Modernization/resources/demopage"
+$pathToRepo = "https://raw.githubusercontent.com/microsoft/MicroHack/refs/heads/main/03-Azure/01-03-Infrastructure/06_Migration_Secure_AI_Ready/resources/demopage/index.html"
 $pathToIndex = "C:\inetpub\wwwroot\index.html"
 $replaceText = "<HOSTNAME>"
 $hostname = $env:computername
